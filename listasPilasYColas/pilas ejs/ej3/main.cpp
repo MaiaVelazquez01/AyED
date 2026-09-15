eea5b0c74d23b0577e09cd298c14b332e8e2ae33
unknown <mvelazquezyarbe@gmail.com>
2026-09-15 02:47:35 -0300

    pilas ej3

---
+++ listasPilasYColas/pilas ejs/ej3/main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+struct Nodo {
+	int info;
+    Nodo* sgte;
+};
+
+void push(Nodo* &pila, int valor);
+int pop(Nodo* &pila);
+void insertarXEnPosicionY(Nodo* &pila, int valor, int pos);
+
+int main() {
+
+    Nodo* pila = NULL;
+
+    push(pila, 1);
+    push(pila, 2);
+    push(pila, 3);
+    push(pila, 4);
+    push(pila, 5);
+    push(pila, 6);
+    push(pila, 7);
+    push(pila, 8);
+    push(pila, 9);
+
+    int x, y;
+
+    cout << "Ingrese el valor a insertar: ";
+    cin >> x;
+    cout << "Ingrese la posicion en la que lo desea ingresar: ";
+    cin >> y;
+    insertarXEnPosicionY(pila, x, y);
+
+    return 0;
+
+}
+
+void push(Nodo* &pila, int valor) {
+    Nodo* nuevo = new Nodo();
+    nuevo->info = valor;
+    nuevo->sgte= pila;
+    pila = nuevo;
+}
+
+int pop(Nodo* &pila) {
+    int ret = pila->info;
+    Nodo* aux = pila;
+    pila = aux->sgte;
+    delete(aux);
+    return ret;
+}
+
+void insertarXEnPosicionY(Nodo* &pila, int valor, int pos) {
+
+    Nodo* aux = NULL;
+    int i = pos;
+
+    if(pila == NULL) {
+
+        cout << "La pila no tiene elementos" << endl;
+
+    } else if(pos == 1) {
+
+        push(pila, valor);
+        cout << "------- Pila resultante -------" << endl;
+        while(pila != NULL) {
+            cout << pop(pila) << endl;
+        }
+
+    } else {
+
+        while(i > 1 && pila != NULL) {
+            push(aux, pila->info);
+            pop(pila);
+            i--;
+        }
+
+        if(pila == NULL) {
+
+            cout << "No existe la posicion " << pos << " en la pila" << endl;
+            while(i < pos) {
+                push(pila, pop(aux));
+                i++;
+            }
+
+        } else {
+
+            push(pila, valor);
+            i = 1;
+            while(i < pos) {
+                push(pila, pop(aux));
+                i++;
+            }
+
+        }
+
+        cout << "------- Pila resultante -------" << endl;
+        while(pila != NULL) {
+            cout << pop(pila) << endl;
+        }
+    }
+
+}
