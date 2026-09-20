8a7e561d3dd43a1bbe986ef176c6d6abd21b4ea2
unknown <mvelazquezyarbe@gmail.com>
2026-09-20 01:07:13 -0300

    colas ej2

---
+++ listasPilasYColas/colas ejs/ej2/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+struct Alumno {
+    int legajo;
+    char nombreApellido[100];
+    char curso[6];
+};
+
+struct Nodo {
+	Alumno alumno;
+    Nodo* sgte;
+};
+
+void agregar(Nodo*&cfte,Nodo*&cfin, Alumno a);
+Alumno suprimir (Nodo*&cfte,Nodo*&cfin);
+int cantidadNodos(Nodo* &cfte, Nodo* &cfin);
+
+int main() {
+
+    Nodo*colafte = NULL;
+	Nodo*colafin = NULL;
+
+    Alumno a1, a2, a3;
+
+    a1.legajo = 1744669;
+    strcpy(a1.nombreApellido, "Maia Velazquez");
+    strcpy(a1.curso, "K5968");
+    a2.legajo = 1744589;
+    strcpy(a2.nombreApellido, "Pepe Gonzalez");
+    strcpy(a2.curso, "K5978");
+    a3.legajo = 1744779;
+    strcpy(a3.nombreApellido, "Ana Benitez");
+    strcpy(a3.curso, "K5869");
+
+    agregar(colafte, colafin, a1);
+    agregar(colafte, colafin, a2);
+    agregar(colafte, colafin, a3);
+
+    cout << "Hay " << cantidadNodos(colafte, colafin) << " nodos en la cola" << endl;
+
+    return 0;
+
+}
+
+void agregar(Nodo* &cfte, Nodo* &cfin, Alumno a) {
+   Nodo*nuevo=new Nodo();
+   nuevo->alumno = a;
+   nuevo->sgte = NULL;
+
+   if(cfte == NULL){
+      cfte = nuevo;
+   } else {
+      cfin->sgte = nuevo;
+   }
+   cfin=nuevo;
+}
+
+Alumno suprimir (Nodo* &cfte, Nodo* &cfin) {
+   Alumno ret;
+   ret = cfte->alumno;
+   Nodo* aux = cfte;
+   cfte = aux->sgte;
+   if(cfte == NULL){
+      cfin = NULL;
+   }
+   delete(aux);
+   return ret;
+}
+
+int cantidadNodos(Nodo* &cfte, Nodo* &cfin) {
+
+    Nodo*cola2fte = NULL;
+	Nodo*cola2fin = NULL;
+    Alumno a;
+    int i = 0;
+    while(cfte != NULL) {
+        a = suprimir(cfte, cfin);
+        i++;
+        agregar(cola2fte, cola2fin, a);
+    }
+
+    while(cola2fte != NULL) {
+        a = suprimir(cola2fte, cola2fin);
+        agregar(cfte, cfin, a);
+    }
+
+    return i;
+
+}
